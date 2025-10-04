4c87ab4cdcd2177ba45de74253180e46b63a8a4c
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-04 12:52:18 +1000

    Add currency images

--- src/currencies.ts
+++ src/currencies.ts
@@ -2,13 +2,39 @@ export type Currency = {
   name: string;
   code: string;
   symbol: string;
+  images: number[];
 };
 
 const currencies: Currency[] = [
-  { name: "Australian dollar", code: "AUD", symbol: "A$" },
-  { name: "Euro", code: "EUR", symbol: "€" },
-  { name: "Pound sterling (British pound)", code: "GBP", symbol: "£" },
-  { name: "US dollar", code: "USD", symbol: "US$" },
-  { name: "Vietnamese Dong", code: "VND", symbol: "₫" },
+  {
+    name: "Australian dollar",
+    code: "AUD",
+    symbol: "A$",
+    images: [0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100],
+  },
+  {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+    images: [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200],
+  },
+  {
+    name: "Pound sterling (British pound)",
+    code: "GBP",
+    symbol: "£",
+    images: [0.01, 0.02, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50],
+  },
+  {
+    name: "US dollar",
+    code: "USD",
+    symbol: "US$",
+    images: [0.01, 0.05, 0.1, 0.25, 1, 2, 5, 10, 20, 50, 100],
+  },
+  {
+    name: "Vietnamese Dong",
+    code: "VND",
+    symbol: "₫",
+    images: [10_000, 20_000, 50_000, 100_000, 200_000, 500_000],
+  },
 ];
 export default currencies;
